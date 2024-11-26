fb113f8f33311425c487a25f86c13343e2b6c244
Josh Brooks <60986161+jtb21091@users.noreply.github.com>
2024-11-25 19:53:53 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,36 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-    const { signOut } = useAuthenticator();
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
+    client.models.Todo.delete({ id })
+      .then(() => setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id)))
+      .catch((error) => console.error("Error deleting todo:", error));
   }
-      <button onClick={signOut}>Sign out</button>
+
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
+      error: (error) => console.error("Error observing todos:", error)
     });
+
+    return () => subscription.unsubscribe();
   }, []);
 
   function createTodo() {
     const content = window.prompt("Todo content");
     if (content) {
-      client.models.Todo.create({ content });
+      client.models.Todo.create({ content })
+        .catch((error) => console.error("Error creating todo:", error));
     }
   }
 
   return (
     <main>
       <h1>My todos</h1>
+      <button onClick={signOut}>Sign out</button>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
