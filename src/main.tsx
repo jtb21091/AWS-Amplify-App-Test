278add8eee0f5eda40654fbf5f913714dd838a7c
Josh Brooks <60986161+jtb21091@users.noreply.github.com>
2024-11-25 19:49:46 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
-Amplify.configure(outputs);
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-  
-  <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
-  </React.StrictMode>
+const client = generateClient<Schema>();
+
+function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id }).then(() => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    }).catch((error) => {
+      console.error("Error deleting todo:", error);
+    });
+  }
+
+  useEffect(() => {
+    const subscription = client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+      error: (error) => console.error("Error observing todos:", error)
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  function createTodo() {
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content }).catch((error) => {
+        console.error("Error creating todo:", error);
+      });
+    }
+  }
+
+  return (
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.content} <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+    </main>
+  );
+}
+
+export default App;
